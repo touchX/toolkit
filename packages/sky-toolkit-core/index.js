ec50aaee47144dc2810ee67eac14f0f004b1dc1d
joebell93 <joe@joebell.co.uk>
2017-07-07 10:40:10 +0100

    [refs #00247] Define compiled CSS Node.js entry points
    Provide access to compiled `sky-toolkit` and `sky-toolkit-core` through
    the package entry points.
    
    For example, inside your layout file:
    
    ```javascript
    const skyToolkitCore = require('sky-toolkit-core');
    
    module.exports = `
    <!DOCTYPE html>
    <!--[if IE 9]><html lang="en-GB" class="ie9"><![endif]-->
    <!--[if gt IE 9]><!-->
    <html lang="en-GB">
    <!--<![endif]-->
        <head>
            <link rel="stylesheet" href="${skyToolkitCore}">
            <!-- Your app's assets -->
            ...
        </head>
        ...
    </html>
    `;
    ```

---
+++ packages/sky-toolkit-core/index.js
@@ -0,0 +1,3 @@
+var version = require('./package.json').version;
+
+module.exports = '//www.sky.com/assets/toolkit-core/v' + version + '/toolkit-core.min.css';
---
+++ packages/sky-toolkit/index.js
@@ -0,0 +1,3 @@
+var version = require('./package.json').version;
+
+module.exports = '//www.sky.com/assets/toolkit/v' + version + '/toolkit.min.css';
