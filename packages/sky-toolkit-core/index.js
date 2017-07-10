9ec4ead812649cb40062db032498b50c2685c41c
joebell93 <joe@joebell.co.uk>
2017-07-10 15:39:29 +0100

    [refs #00251] Pass up sky-toolkit-core's CDN entry point to sky-toolkit
    To avoid the `no-extraneous-dependencies` warning, provide the cdnUrl
    and cdnTag for sky-toolkit-core via sky-toolkit's entry point.

--- packages/sky-toolkit-core/index.js
+++ packages/sky-toolkit-core/index.js
@@ -1,4 +1,4 @@
-var { version } = require('./package.json');
+var version = require('./package.json').version;
 
 var cdnUrl = '//www.sky.com/assets/toolkit-core/v' + version + '/toolkit-core.min.css';
 var cdnTag = '<link rel="stylesheet" href="' + cdnUrl + '">';
--- packages/sky-toolkit/index.js
+++ packages/sky-toolkit/index.js
@@ -1,9 +1,15 @@
-var { version } = require('./package.json');
+var version = require('./package.json').version;
+var skyToolkitCore = require('sky-toolkit-core');
 
 var cdnUrl = '//www.sky.com/assets/toolkit/v' + version + '/toolkit.min.css';
 var cdnTag = '<link rel="stylesheet" href="' + cdnUrl + '">';
 
+var skyToolkitCoreCdnUrl = skyToolkitCore.cdnUrl;
+var skyToolkitCoreCdnTag = skyToolkitCore.cdnTag;
+
 module.exports = {
     cdnUrl,
-    cdnTag
+    cdnTag,
+    skyToolkitCoreCdnUrl,
+    skyToolkitCoreCdnTag
 };
